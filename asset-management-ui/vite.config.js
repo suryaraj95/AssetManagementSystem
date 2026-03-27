aa4d81e969f35fe682897f1cd1b2616e6f958728
Suryaraj Thangaraj <suryaraj.thangaraj@Suryarajs-MacBook-Pro.local>
2026-03-27 08:46:11 +0530

    fix: force esbuild css minifier

--- asset-management-ui/vite.config.js
+++ asset-management-ui/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,7 +8,7 @@ export default defineConfig({
       "@": "/src",
     },
   },
-  css: {
-    minify: 'esbuild'
+  build: {
+    cssMinify: 'esbuild',
   }
 })
